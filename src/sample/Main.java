6b62c3081bc9e735b8384156c875c208be4564d9
Mike Lanier <Mike.Lanier1@comcast.net>
2017-08-31 15:01:01 -0500

    Refactor

---
+++ src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println("start");
+        GridPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        System.out.println(root.getClass().getName());
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
