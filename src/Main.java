3ca85b25817627e39c445a9de4589e42b5ac9a38
Mike Lanier <Mike.Lanier1@comcast.net>
2017-09-01 07:18:24 -0500

    added class mainfrm for building the UI manually rather then through fxml.  Having trouble with data binding in fxml and wanted to move forward with building mazes.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -12,9 +9,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.out.println("start");
-        GridPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		//GridPane root = FXMLLoader.load(getClass().getResource("sample\\sample.fxml"));
+        GridPane root = new mainfrm.mainfrm();
         System.out.println(root.getClass().getName());
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Maze");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
