b5f2364a7748e7e2cee38ae8a88702730e89d4ad
Mike Lanier <Mike.Lanier1@comcast.net>
2017-09-01 07:18:56 -0500

    added class mainfrm for building the UI manually rather then through fxml.  Having trouble with data binding in fxml and wanted to move forward with building mazes.

---
+++ src/mainfrm/mainfrm.java
@@ -0,0 +1,14 @@
+package mainfrm;
+
+import javafx.scene.layout.GridPane;
+
+/**
+ * Created by Mike on 9/1/2017.
+ */
+public class mainfrm extends GridPane
+{
+	public mainfrm()
+	{
+		System.out.println("mainfrm");
+	}
+}
