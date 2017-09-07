6cf882120fa81c1a9d3d2cac5339b9914a65f7f1
Mike Lanier <Mike.Lanier1@comcast.net>
2017-09-07 10:17:01 -0500

    add ObservableMap for cells and walls

---
+++ src/mainfrm/ID.java
@@ -0,0 +1,14 @@
+package mainfrm;
+
+public class ID {
+	public int	xOrigin = 0;
+	public int	yOrigin = 0;
+	public boolean horizonal = true;
+
+	public ID(int _xOrigin, int _yOrigin, boolean _horizonal)
+	{
+		xOrigin = _xOrigin;
+		yOrigin = _yOrigin;
+		horizonal = _horizonal;
+	}
+}
